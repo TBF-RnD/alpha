37282697d642ed0c31a5e87cacdce888ed0b57c9
TBF-RD <trbef@protonmail.com>
2019-05-19 13:06:26 +0300

    test seperately

--- test.js
+++ test.js
@@ -5,13 +5,15 @@
 function getMS(){ return Date.now() }
 
 function logTime(s,t0){
-	 console.log(s+" in "+(getMS()-t0)+"ms")
+	 var str=s+" in "+(getMS()-t0)+"ms"
+	 console.log(str)
+	 return str
 }
 
 function MipMapTst(el){
 	 var canvas=el[0]
 	 var ctx=canvas.getContext('2d')
-	 var max_size=128
+	 var max_size=256
 	 var min_size=6
 
 //	 var m=new MipMap(ctx,"T",max_size,min_size)
@@ -19,42 +21,60 @@ function MipMapTst(el){
 	 var m=new MipMap(ctx,"Q",max_size,min_size)
 	 logTime("new MipMap",t0)
 
-	 // Render all sizes, with bitmaps
-	 var t0=getMS()
-	 m.renderAll(ctx,0,max_size)
-	 logTime("renderAll",t0)
+	 // Test parameters
+	 var testSizes=[220,192,96,48,24,12,6]
+	 var n_iterations=16
 
-	 // Render all sizes, as usual
-	 var t0=getMS()
-	 m.renderAllRef(ctx,400,max_size)
-	 logTime("renderAllReg",t0,400)
+	 if(el.hasClass("plain")){
+		 var P=new Victor(0,max_size)
+	
+	
+		 var P=new Victor(200,max_size*2)
+		 var t1=getMS()
+		 for(var i in testSizes){
+				var size=testSizes[i]
+				var t0=getMS()
+	//			var dim=m.draw(ctx,P,size)
+				for(var j=0;j<n_iterations;j++)
+		 			m.fillToCTX(ctx,"Q",P,size,500)
+				P.y+=size
+	//			logTime("plain "+size,t0)
+		 }
+		 logTime("plain tot",t1)
+	 }
 
-
-	 var P=new Victor(0,max_size*2)
-
-	 // Sizes to test
-	 var testSizes=[220,192,96,48,24,12,10,8,6,5,3]
-	 var P=new Victor(0,max_size*2)
-	 var dim=false
-	 for(var i in testSizes){
-			var size=testSizes[i]
-
-			var t0=getMS()
-			for(var j=0;j<16;j++)
-	 			dim=m.draw(ctx,P,size)
-			P.y+=dim.y
-			logTime("mipmap "+size,t0)
+	 if(el.hasClass("mipmap")){
+		 var P=new Victor(0,max_size)
+		 var dim=false
+		 var t1=getMS()
+		 for(var i in testSizes){
+				var size=testSizes[i]
+	
+				var t0=getMS()
+				for(var j=0;j<n_iterations;j++)
+		 			dim=m.draw(ctx,P,size)
+				P.y+=dim.y
+	//			logTime("mipmap "+size,t0)
+		 }
+		 logTime("mipmap tot:  ",t1)
 	 }
 	 
-	 var P=new Victor(500,max_size*2)
-	 for(var i in testSizes){
-			var size=testSizes[i]
-			var t0=getMS()
-//			var dim=m.draw(ctx,P,size)
-			for(var j=0;j<16;j++)
-	 			m.fillToCTX(ctx,"Q",P,size,500)
-			P.y+=size
-			logTime("plain "+size,t0)
+	 if(el.hasClass("put")){
+		 var mp=new MipMap(ctx,"Q",max_size,min_size)
+		 var P=new Victor(400,max_size)
+		 var dim=false
+		 var t1=getMS()
+		 for(var i in testSizes){
+				var size=testSizes[i]
+	
+				var t0=getMS()
+//				console.log(size)
+				for(var j=0;j<n_iterations;j++)
+		 			dim=mp.put(ctx,P,size)
+				P.y+=size
+	//			logTime("put "+size,t0)
+		 }
+		 logTime("put tot",t1)
 	 }
 }
 
