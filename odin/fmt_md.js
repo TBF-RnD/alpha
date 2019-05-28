5dad3d7630b2e7b5012449fc1755f1152c57e009
TBF-RnD <trbefr@protonmail.com>
2019-05-28 13:17:16 +0300

    new  md  output test
    Fixes to markdown formatting

--- odin/fmt_md.js
+++ odin/fmt_md.js
@@ -3,26 +3,33 @@
  *
  */
 
+function n_symbols(n,c){
+	var ret=""
+	for(var i=0;i<n;i++) ret+=c
+	return ret
+}
+function n_spaces(n){ return n_symbols(n,"  ") } 
+function n_dashes(n){ return n_symbols(n,"-") }
+
 function header(alphabet){
-	var retA="|      |"
-	var retB="|------| "
+	var retA="|   | "
+	var retB="|---|"
 
 	for(var k in alphabet){
 		retA+= alphabet[k] + " | "
-		retB+= "---"
+		retB+= "---|"
 	}
 		
-	return retA+"\n"+retB+"|\n"
+	return retA+"\n"+retB+"\n"
 }
 function rows(rows,alphabet){
 	var ret=""
 	for(var k0 in rows){
 		ret+="| "+k0+" |"
 		for(var k1 in alphabet){
-			console.log("finding  " + alphabet[k1])
 			var  f=rows[k0][alphabet[k1]]
-			if(typeof(rows[k0][alphabet[k1]])=="undefined") f="X"
-			ret+=f+" |"
+			if(typeof(rows[k0][alphabet[k1]])=="undefined") f="0"
+			ret+=" "+f+" |"
 		}
 		ret+="\n"
 	}
@@ -32,16 +39,11 @@ function rows(rows,alphabet){
 function format(dobj){
 	var alphabet=dobj.getAlphabet()
 	var  ret = ""
-	for(var i=0;i<=dobj.degree;i++){		
+	for(var i=1;i<=dobj.degree;i++){		
 		console.log(i+"/"+dobj.degree)
 		ret+=header(alphabet)
 		
 		var level=dobj.d[i]
-		/*
-		console.log("level")
-		console.log(level)
-
-		*/
 		ret+=rows(level,alphabet)
 			
 		ret+="\n"
