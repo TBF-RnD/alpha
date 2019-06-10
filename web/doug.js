b4dcf78dd5bd8d5a15681daebb1732391ecad842
TBF-RnD <trbefr@protonmail.com>
2019-06-10 19:47:18 +0300

    Keybindings on element attr as JSON

--- web/doug.js
+++ web/doug.js
@@ -1,1 +1,43 @@
+//  TODO
+//  - overlaps with sympred.js
+function initDoug(){
+	$("[doug]").each(function(){
+		var el=$(this)
+		var binds=$(this).attr("bindings")
+		var bindo=JSON.parse(binds)
+
+		// get signal finger number from keycode
+		// returns -1 if keycode is not bound
+		function mapKC(which){
+			for(k in bindo){
+				if(bindo[k]==which)
+					return k
+			}
+			return -1
+		}
+
+		el.keydown(function(ev){ 
+			var kc=ev.which
+			var signal=mapKC(kc)
+
+			if(signal==-1)  return
+
+			console.log(signal)
+
+			ev.preventDefault()
+		})
+		el.keyup(function(ev){
+			var kc=ev.which
+			var signal=mapKC(kc)
+			
+			if(signal==-1)  return
+			
+			ev.preventDefault()
+		})
+	})
+}
+
 mobileConsole.init()
+$(document).ready(function(){
+	initDoug()	
+})
